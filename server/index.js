62675d89dc20d98b39ae81e5689a9161487d9d44
Doomer77 <newzibkoff1984@gmail.com>
2022-04-25 22:48:35 +0300

    created connnect db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 7000
 
-app.listen(PORT, () => console.log( `Server run on port ${PORT}`))
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    await app.listen(PORT, () => console.log( `Server run on port ${PORT}`))
+  }catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
