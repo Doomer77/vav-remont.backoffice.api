9d0b3dabdda77ef7dca75722e62beae82d73cd72
Doomer77 <newzibkoff1984@gmail.com>
2022-04-29 16:31:09 +0300

    add developed types of requests for img - items, categories

--- server/routes/imageItemRouter.js
+++ server/routes/imageItemRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
+const imageItemController = require('../controllers/imageItemController')
 
 const router = new Router()
 
-router.post('/')
-router.get('/')
-router.get('/:id')
+router.post('/', imageItemController.create)
+router.get('/', imageItemController.getAll)
+router.get('/:id', imageItemController.getOne)
 
 module.exports = router
